69931aa15ce3b993e3539d898690e66fc31cb274
Jaydin Madore <JaydinMadore@stu.ocsb.ca>
2024-04-17 14:57:18 +0000

    h

--- com/example/ReverseString.java
+++ com/example/ReverseString.java
@@ -7,39 +7,44 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 /**
- * This Program tells the user what a word would be spelt backwards.
+ * This Program tells the user what a 
+ * word would be spelt backwards.
  * Using Input File and Output File.
  * @author Jaydin Madore
  * @version 1.0
  * @since 2024-04-17
  */
 
-// ReverseString class
-public final class ReverseString {
+
+/* 
+ * ReverseString class
+ * 
+*/
+public final class ReverseString{
     // This method reverses a string recursively.
     // It than returns The reversed string.
-    public static String reverse(String str) {
-        if (str.length() == 0) {
+    public static String reverse(String str){
+        if (str.length() == 0){
             return str;
-        } else {
+        } else{
             return reverse(str.substring(1)) + str.charAt(0);
         }
     }
 
- 
     public static void main(final String[] args) {
         // List of file paths to output to.
         String outputFilePath = "output.txt";
         // List of file paths to read from.
         String readFilePath = "input.txt";
-        
+
         try {
             // It reads the files
-            BufferedReader reader = new BufferedReader(new FileReader(readFilePath));
-
-            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath));
+            BufferedReader reader = new BufferedReader
+            (new FileReader(readFilePath));
+            BufferedWriter writer = new BufferedWriter
+            (new FileWriter(outputFilePath));
             String line;
-            while ((line = reader.readLine()) != null) {
+            while ((line = reader.readLine()) != null){
                 String reversedLine = reverse(line);
                 writer.write(reversedLine);
                 writer.newLine();
@@ -47,8 +52,10 @@ public final class ReverseString {
             // Tells the user the program is done running.
             System.out.println("Done");
             System.out.println("Check output File for the results");
-            // This tells the user that there was a Error when trying to read the files 
-            // It could be the input or output file that could be the problme.    
+            // This tells the user that there was 
+            // a Error when trying to read the files
+            // It could be the input or output file 
+            // that could be the problem.
             reader.close();
             writer.close();
         } catch (IOException e) {
